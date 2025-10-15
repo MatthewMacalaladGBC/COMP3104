3891c79f19949670e2e8058305e5f7d80bad1832
Matthew Macalalad <matthew.macalalad@georgebrown.ca>
2025-10-15 17:57:00 -0400

    Dep inclusions (#3)
    * GitHub Action for GitHub pages added, and fixed homepage url in package.json
    
    * minor update to App.js - name change

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
   return (
     <div className="App">
       <h1>Student Information System</h1>
-      <h5>Developed By: 101510305 || Matthew Macalalad || DevOps</h5>
+      <h5>Developed By: 101510305 || Matthew A. Macalalad || COMP3104 || DevOps</h5>
       <StudentForm addStudent={addStudent} />
       <StudentList students={students} />
     </div>
